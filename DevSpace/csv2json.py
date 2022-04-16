3b853f6c3d26cf4cda9739b022abb75dd9928bf2
kashish0603 <kashish.jain2021a@vitstudent.ac.in>
2022-04-16 17:47:49 +0530

    Backend basic

---
+++ DevSpace/csv2json.py
@@ -0,0 +1,22 @@
+import json
+import csv
+
+
+#enter csv file name -------> open("filename", "r")
+with open ("linkedin_scapejobs.csv", "r") as f:
+    reader = csv.reader(f)
+    next(reader)
+    data = []
+    for row in reader:
+        data.append(
+            #enter all the headers from the csv file here along with the row index
+        {
+            "album": row[0],        
+            "year": row[1],
+            "US_peak_chart_post": row[2]
+        }
+    )
+
+#enter json file name ---------> open("filename", "w")
+with open ("linkedin_scapejobs.json", "w") as f:
+    json.dump(data, f, indent=4)
--- DevSpace/google.py
+++ DevSpace/google.py
@@ -2,25 +2,72 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+import pandas as pd
 
-bot1 = webdriver.Chrome("D:\\Work\\C_Programs\\Python\\Drivers\\chromedriver.exe")
+browser = webdriver.Chrome("chromedriver.exe")
 
 url = 'https://www.google.com/search?q=google+jobs&oq=google+jobs&aqs=chrome..69i57.2624j0j9&sourceid=chrome&ie=UTF-8&ibp=htl;jobs&sa=X&ved=2ahUKEwj2h8v-ypb3AhXQ62EKHVKMA6kQutcGKAF6BAgWEBY#htivrt=jobs&htidocid=PLil6aUVWe0AAAAAAAAAAA%3D%3D&fpstate=tldetail'
 query = 'Data Science'
 
-bot1.get(url)
+browser.get(url)
 time.sleep(2)
 
-bot1.find_element(by=By.XPATH,
-                   value='/html/body/header/div[2]/div[2]/div/form/div[1]/div/div/div/div/div/input[1]').click()
+s1 = browser.find_element(by=By.XPATH,
+                   value='/html/body/header/div[2]/div[2]/div/form/div[1]/div/div/div/div/div/input[1]')
+
+print(s1.text)                   
 time.sleep(1)
 
-bot1.find_element(by=By.XPATH,
+browser.find_element(by=By.XPATH,
                   value='/html/body/header/div[2]/div[2]/div/form/button[2]').click()
 time.sleep(1)
-s1 = bot1.find_element(by=By.XPATH,
+s1 = browser.find_element(by=By.XPATH,
                        value='/html/body/header/div[2]/div[2]/div/form/div[1]/div/div/div/div/div/input[1]')
 s1.click()
 time.sleep(1)
 s1.send_keys(query)
 s1.send_keys(Keys.RETURN)
+
+job = browser.find_element_by_class_name("job-card-square__title")
+c=[]
+for i in job:
+    c.append(i.text)
+print(c)
+print() 
+print(len(c))
+
+job_title = []
+for i  in range(len(c)):
+    job_title.append(c[i].strip("Job Title\n"))
+    job_title
+
+print(job_title)
+
+job2 = browser.find_element_by_class_name("job-card-container__company-name")
+comp_name = []
+for i in job2:
+    comp_name.append(i.text)
+print(comp_name)
+print()
+print(len(comp_name))
+
+job3 = browser.find_element_by_class_name("job-card-container__metadata-item")
+loc_name = []
+for i in job3:
+    loc_name.append(i.text)
+print(loc_name)
+print()
+print(len(loc_name))
+
+comp_name.append(" ")
+comp_name.append(" ")
+
+len(comp_name)
+
+col = ["Company Name", "Job Title", "Location"]
+df = pd.DataFrame({"Company Name":comp_name,"Job Title":job_title,"Location":loc_name})
+
+df.head()
+
+csvfile = df.to_csv("linkedin_scapejobs.csv")
+print(csvfile)
